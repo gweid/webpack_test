a225c6a50ec4aba1f50a0e87c1b8f86c1b7cb12e
官维 <guanwei@triumen.com>
2020-03-04 15:51:31 +0800

    优化webpack

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@ document.write("My First Webpack app <br>")
 document.write("优化打包命令 <br>")
 document.write("添加 webpack-dev-server <br>")
 
+let fn = () => {
+    console.log("11111");
+}
+fn()
+
+class A {
+    a = 1
+}
+
+
 addContent()
+
+require('./index.scss')
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
+const os = require('os')
 const path = require("path")
 const webpack = require('webpack')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')  // webpack4 之后使用这个在打包时移除 console.log
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 抽离 css
+const TerserPlugin = require('terser-webpack-plugin') // webpack4 之后使用这个在打包时移除 console.log
+const ProgressBarWebpackPlugin = require('progress-bar-webpack-plugin') // 打包显示进度条
 // const CleanWebpackPlugin = require('clean-webpack-plugin');
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin'); // webpack4 以后的版本需要这样引入
-// const Happypack = require('happypack')
-const os = require('os')
 
+const HappyPack = require('happypack')
 // 获取 cpu 进程
-// const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
+const happyThreadPool = HappyPack.ThreadPool({
+    size: os.cpus().length
+});
+
+// 去除无用样式(如果一些样式开发时没用到，那么这个打包的时候会自动去掉)
+const glob = require('glob')
+const PurgecssWebpackPlugin = require('purgecss-webpack-plugin')
+
+// 动态引入 CDN
+const htmlWebpackExternalsPlugin = require('html-webpack-externals-plugin')
 
 module.exports = {
-    context: path.join(__dirname, "./src"), // 资源入口的路径前缀，必须为绝对路径
+    context: path.resolve(__dirname, "./src"), // 资源入口的路径前缀，必须为绝对路径
     entry: "./index.js", // 入口
     // entry: ['babel-polyfill', './index.js'], // 数组型入口[传入数组的作用，将资源合并(将 babel-polyfill 在 index.js 中引入)，最后一个元素为入口路径]
     // entry: {
@@ -27,7 +38,7 @@ module.exports = {
     // }), // 函数类型入口[好处：可以进行一些动态操作]
 
     output: {
-        path: path.join(__dirname, "dist"), // 出口路径，必须为绝对路径 webpack 4 之后，默认为 dist 目录
+        path: path.resolve(__dirname, "dist"), // 出口路径，必须为绝对路径 webpack 4 之后，默认为 dist 目录
         // filename: "[name].js" // 类似模板语言方式动态生成文件名, 多入口时用到  例如上面多入口，会生成 index.js 和 content.js
         filename: "bundle.js", // 输出文件名
         // publicPath: "", // 指定资源的请求位置
@@ -35,15 +46,17 @@ module.exports = {
         // publicPath: "http://cdn/com/", // 可以配置 cdn
     },
 
-    mode: "development", // mode 为 development时，webpack-dev-server 具备热重载功能
+    mode: "development", // mode 为 development时，webpack-dev-server 具备热重载功能, 两种模式 production development
     // devtoll: 'none', // 在开发者模式下，默认开启 sourcemap, 将其关闭
     devtool: 'cheap-module-eval-source-map', //在开发环境推荐使用，提示比较全，打包速度比较快
     //devtool: 'cheap-module-source-map', // 在生产环境中推荐使用，提示效果会好一些
     devServer: {
         // public: 'http://localhost', // 指定域名，默认 http://localhost:8080
-        // 建议将 devServer.publicPath 和 output.publicPath 的值保持一
+        // 建议将 devServer.publicPath 和 output.publicPath 的值保持一致
         publicPath: "/dist/", // webpack-dev-server 监控路径 (启动 webpack-dev-server 服务，会把资源指向 localhost:3000/assets/，而 webpack 会默认在 dist 目录下，所以应该配置 publicPath 在 /dist/)
         // post: 3000, 监控端口
+        progress: true, // 显示进度条
+        // compress: true, // 开启 zip 压缩
 
         // proxy: {
         //     "/api": {
@@ -53,89 +66,103 @@ module.exports = {
         // }, // 配置代理
 
         hot: true, // 配合开启 HMR，这个必须开启
-        // open: true, // 自动打开浏览器
+        open: true, // 自动打开浏览器
+    },
+    stats: {
+        children: false
     },
 
     module: {
         rules: [{
-            test: '/\.css$/',
-            use: [
-                'style-loader',
-                {
-                    loader: 'css-loader',
+                test: /\.css$/,
+                use: [
+                    // 'style-loader', // 把 css 插入到 head 标签中
+                    MiniCssExtractPlugin.loader, // 抽离 css
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader, // 抽离 css
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2, // 如果 sass 文件里还引入了另外一个 sass 文件，另一个文件还会从 postcss-loader 向上解析。如果不加，就直接从 css-loader 开始解析
+                            modules: true // 开启 css 的模块打包。css样式不会和其他模块发生耦合和冲突
+                        }
+                    },
+                    'postcss-loader', // postcss-loader 配合 postcss.config.js 的 autoprefixer 加入 -webkit, -ms, -o, -moz 这些前缀
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp)$/,
+                use: [{
+                    loader: 'url-loader',
                     options: {
-                        minimize: true, // 开启 css-loader 的压缩功能压缩 css
+                        limit: 8192, //小于 8192k,就可以转化成 base64 格式,大于就会打包成文件格式
+                        name: '[name].[ext]', // [name]_[hash].[ext] // 打包后的图片名字，后缀和打包的之前的图片一样
+                        // outputPath: 'images/', // 输出到那个文件夹下
                     }
-                }
-            ]
-        }, {
-            test: '/\.scss$/',
-            use: [
-                'style-loader',
-                {
-                    loader: 'css-loader',
+                }, {
+                    loader: 'image-webpack-loader', // 图片压缩
                     options: {
-                        importLoaders: 2, // 如果sass文件里还引入了另外一个sass文件，另一个文件还会从postcss-loader向上解析。如果不加，就直接从css-loader开始解析
-                        modules: true // 开启css的模块打包。css样式不会和其他模块发生耦合和冲突
+                        mozjpeg: { // 压缩 jpeg
+                            progressive: true,
+                            quality: 65
+                        },
+                        // optipng: { // 使用 imagemin-optipn 压缩 png, enable: false 为关闭
+                        //     enable: false
+                        // },
+                        pngquant: { // 使用 imagemin-pngquant 压缩 png
+                            quality: '65-90',
+                            speed: 4
+                        },
+                        gifsicle: { // 压缩 gif
+                            interlaced: false,
+                        },
+                        // webp: { // 把 jpg 和 png 压缩为 webp 
+                        //     quality: 75
+                        // }
                     }
-                },
-                'postcss-loader', // postcss-loader 配合 postcss.config.js 的 autoprefixer 加入 -webkit, -ms, -o, -moz 这些前缀
-                'sass-loader'
-            ]
-        }, {
-            test: '/\.(png|jpe?g|gif|webp)$/',
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 8192, //小于 8192k,就可以转化成 base64 格式,大于就会打包成文件格式
-                    name: '[name].[ext]', // [name]_[hash].[ext] // 打包后的图片名字，后缀和打包的之前的图片一样
-                    // outputPath: 'images/', // 输出到那个文件夹下
-                }
-            }, {
-                loader: 'image-webpack-loader', // 图片压缩
-                options: {
-                    mozjpeg: { // 压缩 jpeg
-                        progressive: true,
-                        quality: 65
-                    },
-                    optipng: { // 使用 imagemin-optipn 压缩 png, enable: false 为关闭
-                        enable: false
-                    },
-                    pngquant: { // 使用 imagemin-pngquant 压缩 png
-                        quality: '65-90',
-                        speed: 4
-                    },
-                    gifsicle: { // 压缩 gif
-                        interlaced: false,
-                    },
-                    // webp: { // 把 jpg 和 png 压缩为 webp 
-                    //     quality: 75
-                    // }
-                }
-            }]
-        }, {
-            test: '/\.js$/',
-            exclude: /node_modules/, // node_modules 下的不用 babel-loader 转译
-            use: 'babel-loader'
-        }],
+                }]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, // node_modules 下的不用 babel-loader 转译
+                // use: 'babel-loader', 
+                use: 'happypack/loader?id=js' // happypack 多核优化
+            }
+        ],
     },
 
-    // 利用 happypack 多核提高构建速度
     plugins: [
-        // new Happypack({
-        //     id: "js",
-        //     threadPool: happyThreadPool,
-        //     loaders: []
-        // }),
+        // 利用 happypack 多核提高构建速度
+        new HappyPack({
+            id: "js",
+            threadPool: happyThreadPool,
+            loaders: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'], // 这样才能将 es6 转 es5
+                    plugins: ['@babel/plugin-proposal-class-properties'] // es7 转换
+                }
+            }]
+        }),
 
-        // new HtmlWebpackPlugin({
-        //     filename: 'index.html',
-        //     template: 'src/index.html', // 以index.html为模板，把打包生成的js自动引入到这个html文件中
-        //     minify: { // 压缩 HTML 的配置
-        //         minifyCSS: true, // 压缩 HTML 中出现的 css 代码
-        //         minifyJS: true // 压缩 HTML 中出现的 JS 代码
-        //     },
-        // }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html', // 以 index.html 为模板，把打包生成的 js 自动引入到这个html文件中, 默认就是读取 src 下的，加 ./src/ 会报错
+            minify: { // 压缩 HTML 的配置
+                minifyCSS: true, // 压缩 HTML 中出现的 css 代码
+                minifyJS: true, // 压缩 HTML 中出现的 JS 代码
+                removeComments: true, // 是否去掉注释
+                collapseWhitespace: true, // 折叠成一行
+            },
+            hash: true, // 加 hash 解决缓存问题
+        }),
 
         // 定义全局常量
         new webpack.DefinePlugin({
@@ -149,19 +176,50 @@ module.exports = {
         // 在打包之前，可以删除dist文件夹下的所有内容
         new CleanWebpackPlugin(),
 
-        // 打包时去掉 console 打印、debugger 调试(webpack4 之后不能这样子用，4 以后移除了该方法, 用 minimize)
+        // 打包时去掉 console 打印、debugger 调试( webpack4 之后不能这样子用，4 以后移除了该方法, 用 minimize )
         // new webpack.optimize.UglifyJsPlugin({
         //     compress: {
         //         warnings: false,
         //         drop_debugger: true,
         //         drop_console: true
         //     }
+        // }),
+
+        // 抽离 css
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        }),
+
+        // 打包显示进度条
+        new ProgressBarWebpackPlugin(),
+
+        // 去除无用样式
+        new PurgecssWebpackPlugin({
+            // glob: 同步查找src目录下的任意文件夹下的任意文件,返回一个数组，如['真实路径 /src/css/style.css','真实路径/src/index.js',...]
+            // paths 表示指定要去解析的文件名数组路径
+            // Purgecss 会去解析这些文件然后把无用的样式移除
+            paths: glob.sync("./src/**/*", {
+                nodir: true
+            })
+        }),
+
+        // 动态引入 CDN
+        // new htmlWebpackExternalsPlugin({
+        //     externals: [{
+        //         module: 'vue',
+        //         entry: "https://cdn.bootcss.com/vue/2.6.10/vue.min.js",
+        //         global: 'Vue'
+        //     }]
         // })
     ],
 
-    optimization: {
+    optimization: { // 优化项
         minimizer: [
-            new UglifyJsPlugin(), // uglifyjs-webpack-plugin 进行 js 压缩
+            // uglifyjs-webpack-plugin 进行 js 压缩
+            new UglifyJsPlugin({
+                cache: true, // 开启缓存
+                parallel: true, // 开启多核编译
+            }),
             // 打包时去除 console
             new TerserPlugin({
                 minify: (file, sourceMap) => {
