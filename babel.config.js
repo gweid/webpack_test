4005176fb271b1ef3a8ace6ca75184b2c61292fb
gweid <guanwei006@gmail.com>
2021-03-21 22:35:39 +0800

    babel.config.js抽离babel

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        // useBuiltIns: 'usage'   按需加载
+        useBuiltIns: 'usage',
+        // corejs: 3   指定 corejs 版本
+        corejs: {
+          version: 3,
+        },
+        // targets 具体兼容到哪个浏览器
+        // 或者可以通过 browserslist 来配置浏览器兼容
+        // 如果 targets 和 browserslist 都配置了，targets 会覆盖 browserslist
+        // 实际使用更推荐通过 browserslist 来配置，因为 browserslist 不仅仅是可以 babel 可以使用，postcss 也可以使用
+        // targets: {
+        //   chrome: '58',
+        //   firefox: '40',
+        //   ie: '9',
+        //   edge: '17',
+        //   safari: '10',
+        // },
+      },
+    ],
+  ],
+  plugins: []
+};
